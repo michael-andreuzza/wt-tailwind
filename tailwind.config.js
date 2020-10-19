a0f3ae06b5a1306ffd01a91d0f79e7f62bc6b141
michael-andreuzza <michaelandreuzzavega@gmail.com>
2020-10-19 11:17:49 +0300

    modals

--- tailwind.config.js
+++ tailwind.config.js
@@ -470,6 +470,10 @@ module.exports = {
       '30': '30',
       '40': '40',
       '50': '50',
+      '60': '60',
+      '70': '70',
+      '80': '80',
+      '90': '90',
     },
     gap: theme => theme('spacing'),
     gridTemplateColumns: {
